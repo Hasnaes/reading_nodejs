db5e7c3d874dfe8760805a55a8d47c707c9b8429
Hasnaes <62886913+Hasnaes@users.noreply.github.com>
2020-03-30 14:48:41 +0200

    Model

---
+++ userModel.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(lastname, firstname)
+    {
+        this.lastname = lastname; /* this correspond à l'objet, aux caractéristiques de l'objet */ 
+        this.firstname = firstname;
+    }
+};
+
+module.exports = User; /*export de la classe User*/
