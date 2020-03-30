f41de4fff25fbe6b75c0c2422808594114b377a9
Hasnaes <62886913+Hasnaes@users.noreply.github.com>
2020-03-30 15:25:32 +0200

    Delete userModel.js

--- userModel.js
+++
@@ -1,9 +0,0 @@
-class User {
-    constructor(lastname, firstname)
-    {
-        this.lastname = lastname; /* this correspond à l'objet, aux caractéristiques de l'objet */ 
-        this.firstname = firstname;
-    }
-};
-
-module.exports = User; /*export de la classe User*/
